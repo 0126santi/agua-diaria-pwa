0423ac3da621897e6e040486e6c0b760d10a3f12
Santiago Rodriguez <Srodriguezl.2601@gmail.com>
2025-05-17 00:42:51 -0400

    cambiando inconos y corriengo ruta en vite.config

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: '#00aaff',
         icons: [
           {
-            src: 'public/icons/icon-192.png',
+            src: '/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'public/icons/icon-512.png',
+            src: '/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
